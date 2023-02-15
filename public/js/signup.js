37cf3f4c5e21638ce6247fb9261a50e26047f4a3
Tyler <tylerlvvu@gmail.com>
2023-02-14 21:01:00 -0800

    eigth update

--- public/js/signup.js
+++ public/js/signup.js
@@ -14,10 +14,15 @@ document.querySelector("#signupForm").addEventListener("submit",e=>{
             "Content-Type":"application/json"
         }
     }).then(res=>{
+        console.log(res)
         if(res.ok){
            location.href="/profile"
         } else {
             alert("There was an error signing up")
         }
-    })
+    }).catch(err=>{
+        console.log(err)
+         }
+
+    )
 })
